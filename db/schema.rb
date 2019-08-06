8c209c6c81f2d8ec5dc86ab8c23890538e63d3a2
Lanrna Sayasean <lanrna@bigsouthernsoftware.com>
2019-08-06 10:30:31 -0400

    completed chapter 9, which was to give a more advance login method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_150930) do
+ActiveRecord::Schema.define(version: 2019_08_06_130644) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_05_150930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
